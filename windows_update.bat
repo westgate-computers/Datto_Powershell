aee97d3e0e2f188f053918f172293246a61ab5e2
wchesley <wrchesley1@buffs.wtamu.edu>
2023-08-25 09:32:53 -0500

    feat: install update from cab file

--- windows_update.bat
+++ windows_update.bat
@@ -6,22 +6,30 @@
 :: See Also: https://www.thewindowsclub.com/batch-file-to-download-install-windows-updates
 :: Usage: Grab list of download URL's for each KB you are needing to apply from Microsoft Update Catalogue. 
 :: Copy the first powershell -Command and edit, replacing the current KB download link with yours. 
-:: Copy the wusa.exe line and edit to install teh KB you've downloaded. 
+:: Copy the wusa.exe line and edit to install the KB you've downloaded. 
 ::=======================================================================================
 
 @echo off
 cd /
 :: Copy me to download KB
 echo "Downloading Patches; This will take several minutes depends on your Internet speed"
-powershell -Command "(New-Object Net.WebClient).DownloadFile('http://download.windowsupdate.com/c/msdownload/update/software/uprl/2020/03/windows-kb000000-x64-v5.81_74132082f1421c2217b1b07673b671ceddba20fb.exe', ' kb000000_Scan.exe')"
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://catalog.s.download.windowsupdate.com/c/msdownload/update/software/secu/2023/04/windows10.0-kb5025221-x64_51f5072d3f563a8d58dbaeee2251946b12cc4c0c.cab', 'KB5025221.cab')"
 echo "1 out of 4 downloaded"
 echo "****"
 :: Stop copy here
 
 @echo off
 cd /
-:: Copy me to install KB
-wusa.exe C:\ kb000000.msu /quiet /norestart
-echo " kb000000 is installed, processing the next one"
+:: Copy me to install KB msu file: 
+::wusa.exe C:\ kb000000.msu /quiet /norestart
+::echo " kb000000 is installed, processing the next one"
+::echo "****"
+:: Stop copy here
+
+:: Copy me to install udpates via cab file: 
+dism /online /add-package /packagepath: "C:\KB5025221.cab"
+echo "KB5025221 is installed, processing the next one"
 echo "****"
 :: Stop copy here
+
+echo "Finished applying updates"
