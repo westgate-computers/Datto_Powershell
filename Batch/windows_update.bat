de31189163ee720721271d360664a7e401fc8dab
wchesley <wrchesley1@buffs.wtamu.edu>
2023-09-25 13:32:01 -0500

    fix: update to download win7 patches

--- Batch/windows_update.bat
+++ Batch/windows_update.bat
@@ -13,23 +13,53 @@
 cd /
 :: Copy me to download KB
 echo "Downloading Patches; This will take several minutes depends on your Internet speed"
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://catalog.s.download.windowsupdate.com/c/msdownload/update/software/secu/2023/04/windows10.0-kb5025221-x64_51f5072d3f563a8d58dbaeee2251946b12cc4c0c.cab', 'KB5025221.cab')"
-echo "1 out of 4 downloaded"
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://catalog.s.download.windowsupdate.com/c/msdownload/update/software/secu/2020/08/windows6.1-kb4571719-x64_5288d4c665b6d47f6fa9ff221b4b75a81a907ff1.msu', 'kb4571719.msu')"
+echo "1 out of 3 downloaded"
 echo "****"
 :: Stop copy here
 
 @echo off
 cd /
 :: Copy me to install KB msu file: 
-::wusa.exe C:\ kb000000.msu /quiet /norestart
-::echo " kb000000 is installed, processing the next one"
-::echo "****"
+wusa.exe C:\ kb4571719.msu /quiet /norestart
+echo " kb4571719 is installed, processing the next one"
+echo "****"
+:: Stop copy here
+
+:: Copy me to download KB
+echo "Downloading Patches; This will take several minutes depends on your Internet speed"
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://catalog.s.download.windowsupdate.com/d/msdownload/update/software/secu/2020/09/windows6.1-kb4577053-x64_81540c22784dcd768d027b2577884d8157199da8.msu', 'kb4577053.msu')"
+echo "2 out of 3 downloaded"
+echo "****"
+:: Stop copy here
+
+@echo off
+cd /
+:: Copy me to install KB msu file: 
+wusa.exe C:\ kb4577053.msu /quiet /norestart
+echo " kb4577053 is installed, processing the next one"
+echo "****"
+:: Stop copy here
+
+:: Copy me to download KB
+echo "Downloading Patches; This will take several minutes depends on your Internet speed"
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://catalog.s.download.windowsupdate.com/c/msdownload/update/software/secu/2023/09/windows6.1-kb5030261-x64_e530aefef8e251188b0cfb2762a8996a793792b2.msu', 'kb5030261.msu')"
+echo "3 out of 3 downloaded"
+echo "****"
+:: Stop copy here
+
+@echo off
+cd /
+:: Copy me to install KB msu file: 
+wusa.exe C:\ kb5030261.msu /quiet /norestart
+echo " kb5030261 is installed, processing the next one"
+echo "****"
 :: Stop copy here
 
 :: Copy me to install udpates via cab file: 
-dism /online /add-package /packagepath: "C:\KB5025221.cab"
-echo "KB5025221 is installed, processing the next one"
-echo "****"
+@REM dism /online /add-package /packagepath: "C:\KB5025221.cab"
+@REM echo "KB5025221 is installed, processing the next one"
+@REM echo "****"
 :: Stop copy here
 
 echo "Finished applying updates"
