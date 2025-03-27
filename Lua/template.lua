8a5938a5d6b7e25e8499c99666eec6b087f7c5b7
wchesley <wrchesley1@buffs.wtamu.edu>
2025-03-27 13:41:54 -0500

    feat: add lua scripts
    add lua scripts for extensions in datto ERD

---
+++ Lua/template.lua
@@ -0,0 +1,43 @@
+--[[
+    script_name - brief description
+
+    script_name - Extended Description of script
+
+    ADDITIONAL SOFTWARE NEEDED FOR THIS SCRIPT
+    * none
+
+    USAGE
+    * list usage of scipt
+
+    VERSION
+    * 1.0
+
+    AUTHOR
+    * Walker Chesley
+
+    BUGS, COMMENTS, SUGGESTIONS
+    * Send to Walker Chesley, chesley.walker@gmail.com
+
+    CHANGES
+    * Last Updated: 2025-03-27
+    * Created Script
+]]
+
+-- - - - - - - - - - - - - - - - - - - - - - - -
+-- C O N S T A N T S
+-- - - - - - - - - - - - - - - - - - - - - - - -
+
+-- Define script constants here
+
+
+-- - - - - - - - - - - - - - - - - - - - - - - -
+-- F U N C T I O N S
+-- - - - - - - - - - - - - - - - - - - - - - - -
+
+-- Define script functions here
+
+-- - - - - - - - - - - - - - - - - - - - - - - -
+-- M A I N
+-- - - - - - - - - - - - - - - - - - - - - - - -
+
+-- Main script execution here
