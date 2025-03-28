5dd59decd854a99f2a242e4f17c46bfefe1d352c
wchesley <wrchesley1@buffs.wtamu.edu>
2025-03-28 08:57:56 -0500

    feat: add log helper function

--- Lua/Remove_PDFSkillApp.lua
+++ Lua/Remove_PDFSkillApp.lua
@@ -18,11 +18,12 @@
     * Walker Chesley
 
     BUGS, COMMENTS, SUGGESTIONS
-    * Send to Walker Chesley, chesley.walker@gmail.com
+    * Please use the repository Issues page: https://github.com/westgate-computers/Datto_Powershell/issues
 
     CHANGES
-    * Last Updated: 2025-03-27
+    * Last Updated: 2025-03-28
     * Created Script
+    * Added logMessage(bool,string) function for logging
 ]]
 
 -- - - - - - - - - - - - - - - - - - - - - - - -
@@ -31,10 +32,24 @@
 
 local userProfilePath = "C:\\users"
 local removeTarget = "pdfskills"
+local executionSummary = ""
 
 -- - - - - - - - - - - - - - - - - - - - - - - -
 -- F U N C T I O N S
 -- - - - - - - - - - - - - - - - - - - - - - - -
+
+-- Function to assist with logging, appends logs to executionSummary variable
+-- PARAMETERS:
+-- bool: isError - control error or regular log message, true for error false for normal log
+-- string: logMessage - The message to be logged.
+local function logMessage(isError, logMessage)
+    if isError == true then
+        hunt.error(logMessage)
+    else
+        hunt.log(logMessage)
+    end
+    executionSummary = executionSummary .. logMessage
+end
 
 -- Function to get all directories (user profiles) in C:\Users
 local function getUserProfiles(basePath)
@@ -53,15 +68,15 @@ end
 -- Function to delete a folder recursively
 local function deleteFolder(path)
     if not hunt.fs.path_exists(path) then
-        hunt.error("Path does not exist at" .. path)
+        logMessage(true, "Path does not exist at" .. path)
         return
     end
     local command = 'rmdir /s /q "' .. path .. '"'
     local result = hunt.env.run_powershell(command)
     if result then
-        hunt.log("Folder removed: " .. path)
+        logMessage(false, "SUCCESS: Folder removed: " .. path)
     else
-        hunt.error("Failed to remove folder: " .. path)
+        logMessage(true, "FAILED: Folder not removed: " .. path)
     end
 end
 
@@ -72,8 +87,8 @@ end
 -- Iterate over each user's AppData/Local directory and remove pdfskills directory 
 local profiles = getUserProfiles(userProfilePath)
 for _, profile in ipairs(profiles) do
-    local fullPath = userProfilePath .. "\\" .. profile .. "\\appdata\\" .. removeTarget
+    local fullPath = userProfilePath .. "\\" .. profile .. "\\appdata\\local\\" .. removeTarget
     deleteFolder(fullPath)
 end
 
-hunt.log("Remove PDFSkills script execution complete")
+hunt.summary(executionSummary)
